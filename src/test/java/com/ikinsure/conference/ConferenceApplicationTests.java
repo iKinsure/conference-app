ff3630d0ab9c588a081c65a93687daf1d3e02836
iKinsure <wrotekpl@gmail.com>
2021-05-31 14:48:31 +0200

    ft: init

---
+++ src/test/java/com/ikinsure/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikinsure.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
